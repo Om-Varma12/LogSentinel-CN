91817734c1d137e62ef6933a89384b01817f4e4c
Om Varma <omvarma1210@gmail.com>
2026-03-15 22:14:14 +0530

    feat: agents

--- agents/playbook_agent.py
+++ agents/playbook_agent.py
@@ -9,7 +9,7 @@ llm = ChatGroq(model = "openai/gpt-oss-120b")
 with open('agents/SKILLS/playbook_skill.md', 'r') as f:
     SKILL = f.read()
 
-def summarize_log(log: dict, score: float) -> dict:
+def generate_playbook(log: dict, score: float) -> dict:
     prompt = PROMPT.format(
         log_details = log,
         risk_score = score
@@ -25,4 +25,4 @@ def summarize_log(log: dict, score: float) -> dict:
     
     
 
-# summarize_log({'IP': '233.223.117.90', 'Timestamp': '[27/Dec/2037:12:00:00 +0530]', 'Request Method': 'DELETE', 'Request': '/usr/admin HTTP/1.0', 'Status Code': 502, 'Response Size': 4963, 'Client s/w info': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4380.0 Safari/537.36 Edg/89.0.759.0', 'Request Processing Time': 45}, 59.6)
+# generate_playbook({'IP': '233.223.117.90', 'Timestamp': '[27/Dec/2037:12:00:00 +0530]', 'Request Method': 'DELETE', 'Request': '/usr/admin HTTP/1.0', 'Status Code': 502, 'Response Size': 4963, 'Client s/w info': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4380.0 Safari/537.36 Edg/89.0.759.0', 'Request Processing Time': 45}, 59.6)
